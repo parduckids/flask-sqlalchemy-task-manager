a2c6f2f8e841b9308f435e04cbef693a9cd755e0
Adam V <parduckids@gmail.com>
2024-03-30 15:01:10 +0000

    add task read functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -12,5 +12,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // select init
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
+
+    // collapsibles init
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
   });
     
