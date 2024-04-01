53bc43e3fc69a6c1e82320398f89bb2d7593a49b
Adam V <parduckids@gmail.com>
2024-04-01 14:10:43 +0000

    fix

--- env.py
+++ env.py
@@ -1,8 +1,8 @@
-# import os
+import os
 
-# os.environ.setdefault("IP", "0.0.0.0.")
-# os.environ.setdefault("PORT", "5000")
-# os.environ.setdefault("SECRET_KEY", "fasz22")
-# os.environ.setdefault("DEBUG", "True")
-# os.environ.setdefault("DEVELOPMENT", "True")
-# os.environ.setdefault("DB_URL", "postgresql:///taskmanager")
+os.environ.setdefault("IP", "0.0.0.0.")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "fasz22")
+os.environ.setdefault("DEBUG", "True")
+os.environ.setdefault("DEVELOPMENT", "True")
+os.environ.setdefault("DB_URL", "postgresql:///taskmanager")
